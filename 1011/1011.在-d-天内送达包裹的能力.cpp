d69080ca89d45a558179860b428ecd19a825961d
lee <734675008@qq.com>
2020-07-01 18:31:23 +0800

    对1011题的代码进行了注释

--- 1011/1011.在-d-天内送达包裹的能力.cpp
+++ 1011/1011.在-d-天内送达包裹的能力.cpp
@@ -12,29 +12,29 @@ using namespace std;
 class Solution
 {
 public:
-    void binary_partition(int left, int right, int &result, int &targetDay, vector<int> &weights, vector<int> &currentWeights)
+    void binary_partition(int left, int right, int &result, int &targetDay, vector<int> &currentWeights)
     {
-        int carriage = (left + right) / 2;
-        int day = 0;
-        int currentLoadWeights = 0;
-        for (int i = 0; i < weights.size(); ++i)
+        int carriage = (left + right) / 2;              //当前的轮船负重量，取中间
+        int day = 0;                                    //记录当前carriage最小消耗的天数
+        int currentLoadWeights = 0;                     //记录已经运走的货物重量
+        for (int i = 0; i < currentWeights.size(); ++i) //分配装载
         {
-            if (currentWeights[i] - currentLoadWeights > carriage)
+            if (currentWeights[i] - currentLoadWeights > carriage) //加上第i个货物后超出了此趟运载量，那么前面已经装载好的就可以发车了
             {
-                day++;
-                currentLoadWeights = currentWeights[i - 1];
+                day++;                                      //趟数+1，发车
+                currentLoadWeights = currentWeights[i - 1]; //已经运走的重量进行累加
             }
         }
         if (currentLoadWeights <= *(currentWeights.end() - 1) ||
-            currentLoadWeights <= carriage)
+            currentLoadWeights <= carriage) //"分配装载"循环中，最后一趟的装载还没有发车
             day++;
         if (day > targetDay) //花的天数多了，需要更大的carriage
             left = carriage + 1;
         else //包括与目标天数相等的情况，相等时，要求的carrige一定在包括当前carriage在内往左
             right = carriage;
-        if (left != right)
-            binary_partition(left, right, result, targetDay, weights, currentWeights);
-        else
+        if (left != right) //二分
+            binary_partition(left, right, result, targetDay, currentWeights);
+        else //结果
             result = left = right;
     }
 
@@ -42,14 +42,14 @@ public:
     {
         if (weights.empty())
             return 0;
-        int minCarriage = *max_element(weights.begin(), weights.end());
-        int maxCarriage = accumulate(weights.begin(), weights.end(), 0);
-        vector<int> currentWeights(weights.size());
+        int minCarriage = *max_element(weights.begin(), weights.end());  //可能解的左边界
+        int maxCarriage = accumulate(weights.begin(), weights.end(), 0); //可能解的右边界
+        vector<int> currentWeights(weights.size());                      //weights的累加
         currentWeights[0] = weights[0];
         for (int i = 1; i < weights.size(); ++i)
             currentWeights[i] += currentWeights[i - 1] + weights[i];
-        int result = 0;
-        binary_partition(minCarriage, maxCarriage, result, D, weights, currentWeights);
+        int result = 0;                                                        //存储输出结果
+        binary_partition(minCarriage, maxCarriage, result, D, currentWeights); //二分查找
         return result;
     }
 };
